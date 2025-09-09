0e0a80843608ef4207180e95e05fe4e1725c9f54
Jason Stewart <jason00stewart@gmail.com>
2025-09-09 11:49:53 -0600

    quicksave

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Canopy
 
-[Fast multipole info](https://amath.colorado.edu/faculty/martinss/2014_CBMS/Refs/2012_fmm_encyclopedia.pdf)
+1. [Fast multipole info](https://amath.colorado.edu/faculty/martinss/2014_CBMS/Refs/2012_fmm_encyclopedia.pdf)
 
-[Lecture 2](https://amath.colorado.edu/faculty/martinss/2014_CBMS/Lectures/lecture02.pdf)
+2. [Lecture 2](https://amath.colorado.edu/faculty/martinss/2014_CBMS/Lectures/lecture02.pdf)
 
-[FMM for Vortical Flows](https://repositorio.unesp.br/server/api/core/bitstreams/0e824479-3128-41f7-8cd2-462e9a242c42/content)
+3. [FMM for Vortical Flows](https://repositorio.unesp.br/server/api/core/bitstreams/0e824479-3128-41f7-8cd2-462e9a242c42/content)
 
-[1987_greengard_dissertation](https://amath.colorado.edu/faculty/martinss/2014_CBMS/Refs/1987_greengard_dissertation.pdf)
+4. [1987_greengard_dissertation](https://amath.colorado.edu/faculty/martinss/2014_CBMS/Refs/1987_greengard_dissertation.pdf)
--- src/Canopy_Kernels.hpp
+++ src/Canopy_Kernels.hpp
@@ -34,75 +34,39 @@ namespace Kernel
 {
 
 //---------------------------------------------------------------------------//
+using cdouble = std::complex<double>;
+const double PI = 3.141592653589793;
 
-template <class ScalarType>
-struct Triple {
-    ScalarType x, y, z;
-
-    KOKKOS_INLINE_FUNCTION
-    Triple()
-        : x(ScalarType(0)), y(ScalarType(0)), z(ScalarType(0)) {}
-
-    KOKKOS_INLINE_FUNCTION
-    Triple& operator+=(const Triple& rhs) {
-        x += rhs.x;
-        y += rhs.y;
-        z += rhs.z;
-        return *this;
-    }
-};
-
+// Spherical harmonics (complex, condon-shortley phase)
 /**
- * Aggregates the first slice (positions) based on average and 
- * the second slice based on sum.
+ * Compute the complex spherical harmonic Y_(n, m) (theta, phi)
+ * Where:
+ *  theta is the polar angle 
+ *  phi is the azimuthal angle
+ * in spherical coorindates
+ * Per equation 3.32 in source 4.
  */
-template <class MemorySpace, class ExecutionSpace, class AoSoAType>
-struct KernelFunction {
-public:
+cdouble Ynm(int n, int m, double theta, double phi)
+{
+    // Associated Legendre P_n^m using std::assoc_legendre (C++17)
+    // NOTE: std::assoc_legendre(n, m, x) with x=cos(theta)
+    double x = std::cos(theta);
+    double Pnm = std::assoc_legendre(n, std::abs(m), x);
 
-    using memory_space = MemorySpace;
-    using execution_space = ExecutionSpace;
-    using aosoa_type = AoSoAType;
-    using member_types = typename AoSoAType::member_types;
+    // Normalization term
+    double norm = std::sqrt( ((2.0*n+1)/(4.0*PI)) *
+                             std::tgamma(n-std::abs(m)+1)/std::tgamma(n+std::abs(m)+1) );
+    
+                             // e^(i m phi)
+    cdouble eimphi = std::polar(1.0, m*phi);
 
-    KernelFunction() 
-    {
-        _avgs = aosoa_type("avgs", 1);
+    if (m < 0) {
+        // Condon–Shortley phase: Y_n^{-m} = (-1)^m conj(Y_n^m)
+        return std::pow(-1.0, m) * std::conj(norm * Pnm * eimphi);
     }
+    return norm * Pnm * eimphi;
+}
 
-    aosoa_type _avgs;
-
-    aosoa_type vals() {return _avgs;}
-
-    void operator()(const aosoa_type& data) const
-    {
-        std::size_t data_size = data.size();
-
-        auto slice0 = Cabana::slice<0>(data);
-
-        // Calculate average position of slice 0
-        Triple<double> sum;
-        Kokkos::parallel_reduce(
-            "aggregate_xyz",
-            Kokkos::RangePolicy<execution_space>(0, data_size),
-            KOKKOS_LAMBDA(const int i, Triple<double>& local_sum) {
-                local_sum.x += slice0(i, 0);
-                local_sum.y += slice0(i, 1);
-                local_sum.z += slice0(i, 2);
-            }, sum );
-        
-        sum.x /= static_cast<double>(data_size);
-        sum.y /= static_cast<double>(data_size);
-        sum.z /= static_cast<double>(data_size);
-
-        Cabana::Tuple<member_types> tp;
-        Cabana::get<0>( tp, 0 ) = sum.x;
-        Cabana::get<0>( tp, 1 ) = sum.y;
-        Cabana::get<0>( tp, 2 ) = sum.z;
-
-        _avgs.setTuple(0, tp);
-    }
-};
 
 
 } // end namespace Kernel
