05054572bd1d3aa94a0f4df07e5513edfa010f96
Jason Stewart <jason00stewart@gmail.com>
2025-09-10 13:57:56 -0600

    ran clang-format

--- tests/tstKernels.hpp
+++ tests/tstKernels.hpp
@@ -27,86 +27,94 @@ void scalarP2MKernel()
 {
     // Create points and q (scalar value)
     const int num_points = 20;
-    Kokkos::View<double*[3], TEST_MEMSPACE> cart_coords("cart_coords", num_points);
-    Kokkos::View<double*, TEST_MEMSPACE> q("q", num_points);
+    Kokkos::View<double* [3], TEST_MEMSPACE> cart_coords( "cart_coords",
+                                                          num_points );
+    Kokkos::View<double*, TEST_MEMSPACE> q( "q", num_points );
 
     using RandomPool = Kokkos::Random_XorShift64_Pool<TEST_EXECSPACE>;
-    RandomPool rand_pool(12345); // Seed the random number generator
-    Kokkos::parallel_for("populate_cart_coords", Kokkos::RangePolicy<TEST_EXECSPACE>(0, num_points),
-        KOKKOS_LAMBDA(const int i) {
+    RandomPool rand_pool( 12345 ); // Seed the random number generator
+    Kokkos::parallel_for(
+        "populate_cart_coords",
+        Kokkos::RangePolicy<TEST_EXECSPACE>( 0, num_points ),
+        KOKKOS_LAMBDA( const int i ) {
             auto rand_gen = rand_pool.get_state();
-            cart_coords(i, 0) = rand_gen.drand() * 2.0 - 1.0;
-            cart_coords(i, 1) = rand_gen.drand() * 2.0 - 1.0;
-            cart_coords(i, 2) = rand_gen.drand() * 2.0 - 1.0;
-            q(i) = cart_coords(i, 0) * 1.5;
-            rand_pool.free_state(rand_gen);
-        });
+            cart_coords( i, 0 ) = rand_gen.drand() * 2.0 - 1.0;
+            cart_coords( i, 1 ) = rand_gen.drand() * 2.0 - 1.0;
+            cart_coords( i, 2 ) = rand_gen.drand() * 2.0 - 1.0;
+            q( i ) = cart_coords( i, 0 ) * 1.5;
+            rand_pool.free_state( rand_gen );
+        } );
     Kokkos::fence();
 
     // Expansion center
-    Kokkos::Array<double, 3> expansion_center = {0.1, -0.6, 0.3};
+    Kokkos::Array<double, 3> expansion_center = { 0.1, -0.6, 0.3 };
 
     // Target point
     double Px = 13.6, Py = -5.1, Pz = 2.0;
     double r, theta, phi;
-    Canopy::Kernel::cart2sph(Px-expansion_center[0], Py-expansion_center[1], Pz-expansion_center[2], r, theta, phi);
+    Canopy::Kernel::cart2sph( Px - expansion_center[0],
+                              Py - expansion_center[1],
+                              Pz - expansion_center[2], r, theta, phi );
 
     // Direct potential
-    auto cart_coords_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), cart_coords);
-    auto q_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), q);
+    auto cart_coords_host =
+        Kokkos::create_mirror_view_and_copy( Kokkos::HostSpace(), cart_coords );
+    auto q_host = Kokkos::create_mirror_view_and_copy( Kokkos::HostSpace(), q );
     double phi_direct = 0.0;
     double max_rho = 0.0; // for error bound
-    for (int i=0; i<num_points; ++i) {
-        double dx = Px - cart_coords_host(i, 0);
-        double dy = Py - cart_coords_host(i, 1);
-        double dz = Pz - cart_coords_host(i, 2);
-        double dist = std::sqrt(dx*dx+dy*dy+dz*dz);
-        phi_direct += q_host(i) / dist;
+    for ( int i = 0; i < num_points; ++i )
+    {
+        double dx = Px - cart_coords_host( i, 0 );
+        double dy = Py - cart_coords_host( i, 1 );
+        double dz = Pz - cart_coords_host( i, 2 );
+        double dist = std::sqrt( dx * dx + dy * dy + dz * dz );
+        phi_direct += q_host( i ) / dist;
 
         // distance from expansion center for error estimate
-        double ddx = cart_coords_host(i,0) - expansion_center[0];
-        double ddy = cart_coords_host(i,1) - expansion_center[1];
-        double ddz = cart_coords_host(i,2) - expansion_center[2];
-        double rho = std::sqrt(ddx*ddx + ddy*ddy + ddz*ddz);
-        max_rho = std::max(max_rho, rho);
+        double ddx = cart_coords_host( i, 0 ) - expansion_center[0];
+        double ddy = cart_coords_host( i, 1 ) - expansion_center[1];
+        double ddz = cart_coords_host( i, 2 ) - expansion_center[2];
+        double rho = std::sqrt( ddx * ddx + ddy * ddy + ddz * ddz );
+        max_rho = std::max( max_rho, rho );
     }
 
     constexpr auto pi = Kokkos::numbers::pi_v<double>;
 
     // Loop over truncation degree
-    for (int p = 1; p <= 5; ++p)
+    for ( int p = 1; p <= 5; ++p )
     {
-        Canopy::Kernel::Scalar::P2M<TEST_MEMSPACE, TEST_EXECSPACE> kernel(p);
+        Canopy::Kernel::Scalar::P2M<TEST_MEMSPACE, TEST_EXECSPACE> kernel( p );
 
         // Particle to multipole calcuation performed in operator
-        auto M = kernel(cart_coords, q, num_points, expansion_center);
-        auto M_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), M);
+        auto M = kernel( cart_coords, q, num_points, expansion_center );
+        auto M_host =
+            Kokkos::create_mirror_view_and_copy( Kokkos::HostSpace(), M );
 
-        // Perform multipole to particle conversation to calculate potential at target
-        // Equation 3.36 in source 4
+        // Perform multipole to particle conversation to calculate potential at
+        // target Equation 3.36 in source 4
         using cdouble = Kokkos::complex<double>;
         cdouble phi_multipole = 0.0;
-        for (int n=0; n<=p; ++n) {
-            double pref = 4*pi / double(2*n + 1);
-            for (int m=-n; m<=n; ++m) {
-                int idx = Canopy::Kernel::Scalar::index(n,m);
-                phi_multipole += pref * M_host(idx) / Kokkos::pow(r,n+1) *
-                                 Canopy::Kernel::Scalar::Ynm(n,m,theta,phi);
+        for ( int n = 0; n <= p; ++n )
+        {
+            double pref = 4 * pi / double( 2 * n + 1 );
+            for ( int m = -n; m <= n; ++m )
+            {
+                int idx = Canopy::Kernel::Scalar::index( n, m );
+                phi_multipole +=
+                    pref * M_host( idx ) / Kokkos::pow( r, n + 1 ) *
+                    Canopy::Kernel::Scalar::Ynm( n, m, theta, phi );
             }
         }
 
-        double error = std::abs(phi_multipole.real() - phi_direct);
-        double bound = std::pow(max_rho/r, p+1) * std::abs(phi_direct);
+        double error = std::abs( phi_multipole.real() - phi_direct );
+        double bound = std::pow( max_rho / r, p + 1 ) * std::abs( phi_direct );
 
         // Check that the error is within 10*bound, which accounts
         // for imprecision due to imtermediate rounding.
-        EXPECT_NEAR(phi_multipole.real(), phi_direct, 10*bound)
-            << "p=" << p
-            << " multipole=" << phi_multipole.real()
-            << " direct=" << phi_direct
-            << " error=" << error
-            << " bound~" << bound
-            << std::endl;
+        EXPECT_NEAR( phi_multipole.real(), phi_direct, 10 * bound )
+            << "p=" << p << " multipole=" << phi_multipole.real()
+            << " direct=" << phi_direct << " error=" << error << " bound~"
+            << bound << std::endl;
     }
 }
 
