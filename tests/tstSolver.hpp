7209c3794e17423396d6fb518ec8a178a86c7541
Jason Stewart <jason00stewart@gmail.com>
2026-03-24 16:28:21 -0600

    Updated to use new solve API

--- tests/tstSolver.hpp
+++ tests/tstSolver.hpp
@@ -52,17 +52,6 @@ void testSolver(int points_per_proc_in, bool balanced)
     // if (rank == 0) printf("R%d: num tree layers: %d\n", rank, tree->numLayers());
     // ASSERT_EQ(tree->numLayers(), 3) << "testMultipole2Local: Error: Solver depth must be depth 3.";
 
-    // Check mesh information for leaf layer
-    int cells_per_dimension_leaf = cells_per_tile * leaf_tiles;
-    Kokkos::Array<scalar_type, 3> cell_size;
-    for (int i = 0; i < 3; ++i)
-    {
-        cell_size[i] = (global_high_corner[i] - global_low_corner[i]) / cells_per_dimension_leaf;
-    }
-    ASSERT_EQ(tree->layer(0)->cellsPerDim(), cells_per_dimension_leaf) << "testMultipole2Local: Error: Unexpected cells_per_leaf_dimension";
-    ASSERT_EQ(tree->layer(0)->tilesPerDim(), leaf_tiles) << "testMultipole2Local: Error: Unexpected leaf_tiles";
-    ASSERT_EQ(tree->layer(0)->cellSize(), cell_size) << "testMultipole2Local: Error: Unexpected cell_size";
-
     // Create the data on rank 0. It will automatically be distributed correctly when
     // filled into the tree. There must be enough particles so that the target point resides
     // in a cell that has been activated in the mesh. This won't be a problem in the
@@ -162,16 +151,29 @@ void testSolver(int points_per_proc_in, bool balanced)
         }
     }
 
-    // Copy to device
-    auto particle_aosoa =
-        Cabana::create_mirror_view_and_copy( TEST_MEMSPACE(), particle_aosoa_host );
+    // Copy particles to device
+    auto aosoa_device = std::make_shared<particle_aosoa_type_f>("aosoa_device", particle_aosoa_host.size());
+    Cabana::deep_copy(*aosoa_device, particle_aosoa_host);
         
     // Fill the tree. This migrates particles to their correct rank.
     bool run_load_balance = !balanced;
-    tree->solve(particle_aosoa, run_load_balance);
+    tree->solve(aosoa_device, run_load_balance);
+
+    // Check mesh information for leaf layer. Must be done after solve or else the tree has
+    // not yet been constructed.
+    int cells_per_dimension_leaf = cells_per_tile * leaf_tiles;
+    Kokkos::Array<scalar_type, 3> cell_size;
+    for (int i = 0; i < 3; ++i)
+    {
+        cell_size[i] = (global_high_corner[i] - global_low_corner[i]) / cells_per_dimension_leaf;
+    }
+    ASSERT_EQ(tree->layer(0)->cellsPerDim(), cells_per_dimension_leaf) << "testMultipole2Local: Error: Unexpected cells_per_leaf_dimension";
+    ASSERT_EQ(tree->layer(0)->tilesPerDim(), leaf_tiles) << "testMultipole2Local: Error: Unexpected leaf_tiles";
+    ASSERT_EQ(tree->layer(0)->cellSize(), cell_size) << "testMultipole2Local: Error: Unexpected cell_size";
+
 
     // Gather all particles from the tree back to rank 0 for testing
-    auto tmp = Cabana::create_mirror_view_and_copy(Kokkos::HostSpace(), tree->data());
+    auto tmp = Cabana::create_mirror_view_and_copy(Kokkos::HostSpace(), *(tree->data()));
     particle_aosoa_type_f_h tree_particles("tree_particles", tmp.size());
     Cabana::deep_copy(tree_particles, tmp);
 
@@ -224,11 +226,11 @@ void testSolver(int points_per_proc_in, bool balanced)
 
 TEST( Solver, testSolver_balanced )
 { 
-    testSolver<6>(50, true);
+    testSolver<6>(500, true);
 }
 TEST( Solver, testSolver_unbalanced )
 { 
-    testSolver<6>(50, false);
+    testSolver<6>(500, false);
 }
 
 //---------------------------------------------------------------------------//
