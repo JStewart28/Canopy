146517d10d25b2849ff93f1dcb8d843ff05c6520
Jason Stewart <jason00stewart@gmail.com>
2026-05-22 12:24:53 -0600

    Added fences to debug hangs

--- examples/03_gravity_solve/gravity_solve.cpp
+++ examples/03_gravity_solve/gravity_solve.cpp
@@ -209,6 +209,8 @@ int main( int argc, char* argv[] )
             integrate_particles( particles, solver.gradient(),
                                  solver.num_local_particles(), dt, G );
 
+            Kokkos::fence();
+
             actions[step] =
                 solver.auto_maintain<Position, Mass>( particles );
 
